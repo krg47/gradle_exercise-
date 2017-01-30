782b83878886e180fb55db049972d84857937c8b
Nicole Daniels <nzoner12@yahoo.com>
2017-01-30 10:21:04 -0500

    Finish implementation

--- gradlestuff.java
+++ gradlestuff.java
@@ -1,45 +1,50 @@
-
 import java.util.*;
-
-
 
 public class gradlestuff{
 
-
 	public static void main(String[] args){
-
 		
-		boolean valid = false;
 		String arg0 = args[0];
 		arg0 = arg0.toLowerCase();
 		if (!(arg0.equals("triangle") || arg0.equals("lazy"))){
-			
 			System.exit(1);
 		}
-		if (!args[1) instanceof int){
-			System.exit(2);
-		}
-		if (args[1] < 0){
+
+    try {
+      Integer.parseInt(args[1]);
+    }
+    catch(NumberFormatException e) {
+      System.exit(2);
+    }
+
+		if (Integer.parseInt(args[1]) < 0){
 			System.exit(2);
 		}
 
 		if (args[0].equals("triangle")){
-			triangle(args[1]);
+			int solution = triangle(Integer.parseInt(args[1]));
+      printSolution(Integer.parseInt(args[1]), solution, "Tri");
 		}
+
 		if (args[0].equals("lazy")){
-			lazy(args[1]);
+			int solution = lazy(Integer.parseInt(args[1]));
+      printSolution(Integer.parseInt(args[1]), solution, "Lazy");
 		}
-
-
-
 	}
 
-	public static int lazy(int n){
+  public static void printSolution(int startNum, int endNum, String algo){
+    System.out.println(algo + "(" + startNum + ") = " + endNum);
+  }
 
+	public static int lazy(int n){
 		int temp = n * n;
 		temp += n + 2;
 		temp /=2;
-		return temp;
-
+    return temp;
 	}
+
+  public static int triangle(int n){
+    int triangular = n * (n + 1)/2;
+    return triangular;
+  }
 }
